e2f79d58e2ab566bcaa9315f9020640c503077b6
Christopher <christopher.ferraro34@gmail.com>
2024-01-28 18:09:52 -0500

    routes complete
    routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,7 +17,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Product.findByPk(req.params.id, {include: ({model: Product}) });
+    const tagData = await Tag.findByPk(req.params.id, {include: ({model: Product}) });
     if (!tagData) {
       res.status(404).json({ message: 'No tag matching this ID.'})
       return;
@@ -38,9 +38,18 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
-  
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.delete('/:id', async (req, res) => {
